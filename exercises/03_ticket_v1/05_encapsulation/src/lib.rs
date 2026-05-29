2565f50e5544d2f2a2987c9dca23e9ad383d47cb
multilobyte <multilobyte@hotmail.com>
2026-05-29 00:03:40 -0700

    06_ownership

--- exercises/03_ticket_v1/05_encapsulation/src/lib.rs
+++ exercises/03_ticket_v1/05_encapsulation/src/lib.rs
@@ -32,8 +32,11 @@ pub mod ticket {
 
         // TODO: Add three public methods to the `Ticket` struct:
         //  - `title` that returns the `title` field.
+        pub fn title(&self) -> &str { &self.title }
         //  - `description` that returns the `description` field.
+        pub fn description(&self) -> &str { &self.description }
         //  - `status` that returns the `status` field.
+        pub fn status(&self) -> &str { &self.status }
     }
 }
 
